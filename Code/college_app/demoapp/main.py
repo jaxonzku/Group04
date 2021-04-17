8c138bc55a6037b39403c8f9d8015882e19e2107
jaxonzku <37749192+jaxonzku@users.noreply.github.com>
2021-04-17 13:29:00 +0530

    python script update

--- Code/college_app/demoapp/main.py
+++ Code/college_app/demoapp/main.py
@@ -1,10 +1,23 @@
 #C:\Users\jackson\AppData\Local\Programs\Python\Python39\python.exe
-import os,re
 
+import os,re
+from os import path
 from pathlib import Path
 import subprocess
 
+##################0########
+
 constants=['inp1','inp2','inp3']
+try:
+    os.remove(r"C:\xampp\htdocs\college_app\demoapp\build\app\outputs\apk\release\app-release.apk")
+except :
+    print("file not present")
+
+
+print ("1st test File exists:"+str(path.exists(r'C:\xampp\htdocs\college_app\demoapp\build\app\outputs\apk\release\app-release.apk')))
+
+
+
 
 
 def obtainer():
@@ -65,7 +78,7 @@ for i in range(len(data)):
 
 process=subprocess.Popen(["powershell","flutter build apk --build-name=1.0.1 --build-number=1"],stdout=subprocess.PIPE);
 result=process.communicate()[0]
-print (result)
+print (result,)
 
 
 
@@ -75,4 +88,4 @@ for i in range(len(data)):
     #print(i)
     reverser(str(data[i]),str(constants[i]))
 
-
+print ("File exists:"+str(path.exists(r'C:\xampp\htdocs\college_app\demoapp\build\app\outputs\apk\release\app-release.apk')))
