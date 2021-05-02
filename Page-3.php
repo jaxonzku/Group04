1bbbde84432d4ed01f75351a37e0a8174dd976a6
DIVYA-PETER <66404545+DIVYA-PETER@users.noreply.github.com>
2021-05-02 14:53:38 +0530

    Add files via upload

---
+++ Page-3.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Give Us The Necessary Details Of Your Institution, Please Fill The Form Without Mistakes">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 2</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Page-2.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.11.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Page 2">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-header u-header" id="sec-73d1"><div class="u-clearfix u-sheet u-sheet-1">
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.php" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-2.html" style="padding: 10px 20px;">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-3.php" style="padding: 10px 20px;">Page 2</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-1.php" style="padding: 10px 20px;">Page 3</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.php">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Contact.html">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-2.html">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-3.php">Page 2</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-1.php">Page 3</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1980" data-image-height="1114" id="sec-2dc4">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-title u-text-1">Give Us The Necessary Details Of Your Institution</h1>
+        <blockquote class="u-text u-text-2">These details will be used to generate your application.It may take upto 10 minutes for the app generation.Kindly be patient while it completes.</blockquote>
+        <div class="u-align-center u-black u-container-style u-expanded-width-xs u-group u-opacity u-opacity-35 u-group-1">
+          <div class="u-container-layout u-valign-middle-xs u-container-layout-1">
+            <h1 class="u-text u-text-3">Please Fill The Form Without Mistakes</h1>
+            <div class="u-align-left u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-form u-form-1">
+              <form action="page-1.php" method="POST" class="u-clearfix u-form-custom-backend u-form-spacing-28 u-form-vertical u-inner-form" style="padding: 10px" source="custom" name="form" redirect="true">
+                <div class="u-form-group u-form-name u-form-group-5">
+                  <label for="name-5a14" class="u-label" wfd-invisible="true">Upload Logo</label>
+                  <?php
+                    if (isset($_POST['upload'])) {
+                        $file_name = $_FILES['file']['name'];
+                        $file_type = $_FILES['file']['type'];
+                        $file_tem_loc = $_FILES['file']['tmp_name'];
+                        $file_tem_loc1 = $_FILES['file']['tmp_name'];
+                        $file_size = $_FILES['file']['size'];
+                        $file_store = "image/".$file_name;
+
+                        move_uploaded_file($file_tem_loc, $file_store);
+
+                    }
+                 ?>
+                  <input type="file" name="file"/>
+                  <input type="submit" id="name-5a14" name="upload" class="u-input u-input-rectangle u-white" value="Upload Logo" required="" >
+                </div>
+                <div class="u-form-group u-form-group-6">
+                  <label for="text-ab3d" class="u-label">Upload College Image</label>
+                  <input type="file" name="file"/>
+                  <input type="submit" id="name-5a14" name="upload" class="u-input u-input-rectangle u-white" value="Upload Image" required="" >
+                </div>
+                <div class="u-form-group u-form-group-7">
+                  <label for="text-ab3d" class="u-label">Download this Excel file and fill  </label>
+                  <a href="image\adsxa.jpg" class="u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-radius-20 u-btn-9" download>Download<br>
+                  </a>
+                </div>
+                <div class="u-form-group u-form-group-8">
+                  <label for="text-ab3d" class="u-label">Upload File Here </label>
+                  <input type="file" name="file"/>
+                  
+                  <input type="submit" id="name-5a14" name="upload" class="u-input u-input-rectangle u-white" value="Upload File" required="" >
+                </div>
+                <div class="u-align-center u-form-group u-form-submit u-form-group-4">
+                  <a href="image\adsxa.jpg" class="u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-radius-20 u-btn-1" download>Generte Application<br>
+                  </a>
+                  <input type="submit" action="Page-1.php" value="submit" class="u-form-control-hidden" name="button1" wfd-invisible="true">
+                </div>
+                <div class="u-form-send-message u-form-send-success" wfd-invisible="true"> Thank you! Your message has been sent. </div>
+                <div class="u-form-send-error u-form-send-message" wfd-invisible="true"> Unable to send your message. Please fix errors then try again. </div>
+                <input type="hidden" value="" name="recaptchaResponse" wfd-invisible="true">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-2" src="" data-image-width="1e980" data-image-height="1237" id="sec-cdd8">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-3" src="" data-image-width="1080" data-image-height="1080" id="sec-a14b">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-title u-text-1">Sample Headline</h1>
+        <p class="u-large-text u-text u-text-variant u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div>
+    </section>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-4" src="" data-image-width="1080" data-image-height="1080" id="sec-1257">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-title u-text-1">Sample Headline</h1>
+        <p class="u-large-text u-text u-text-variant u-txt-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-60b8"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/html-website-builder" target="_blank">
+        <span>Visual HTML Editor</span>
+      </a>. 
+    </section>
+  </body>
+</html>
